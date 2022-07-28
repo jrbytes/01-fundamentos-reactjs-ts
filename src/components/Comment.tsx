3d7cb800a3211bd105ca568f22e4273239ea7c7e
Jr Bytes <itamazor@gmail.com>
2022-07-28 11:03:44 -0300

    extensao de interfaces para tipagem padrao de uma tag

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -23,7 +23,10 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
   
   return(
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/jrbytes.png" />
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/jrbytes.png"
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
